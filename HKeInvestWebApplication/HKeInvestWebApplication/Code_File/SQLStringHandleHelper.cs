672a3a87205a0f7915ac24f44f878df4f99a485d
ZENGXH <xzengaf@ust.hk>
2016-05-01 20:51:20 +0800

    can create account now

---
+++ HKeInvestWebApplication/HKeInvestWebApplication/Code_File/SQLStringHandleHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace HKeInvestWebApplication
+{
+    public class SQLStringHandleHelper
+    {
+        public string handleString(string data)
+        {
+            data = data.Replace("'", "''");
+            return data;
+        }
+        public string backString(string data)
+        {
+            data = data.Replace("''", "'");
+            return data;
+        }
+    }
+}
